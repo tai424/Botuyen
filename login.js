020183054332a27214df8a5996ae472ee2e843e0
tai424 <148871478+tai424@users.noreply.github.com>
2025-03-23 21:20:39 +0700

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ const fs = require("fs-extra");
 const readline = require("readline");
 const totp = require("totp-generator");
 const login = require("helyt");
-login({email: "100068910213508", password: "123456a@"}, (err, api) => {
+login({email: "100072792229844", password: "123456789acc"}, (err, api) => {
     if(err) return console.error(err);
     const json = JSON.stringify(api.getAppState());
     fs.writeFileSync(`./${config.APPSTATEPATH}`, json);
